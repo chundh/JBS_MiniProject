f2443cb26459bcd4a4b515d35312d83de60ab904
LNoah <limnoah0301@gmail.com>
2020-10-19 11:08:05 +0900

    test

---
+++ madeMaze/src/com/company/Thread/TimeThread.java
@@ -0,0 +1,15 @@
+package com.company.Thread;
+
+public class TimeThread extends Thread {
+    @Override
+    public void run() {
+        //쓰레드에서 실행할 코드를 작성
+
+        setTimer();
+    }
+
+    public void setTimer(){
+
+
+    }
+}
